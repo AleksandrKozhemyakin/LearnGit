36ebb39522a892d9f4d4d0c79760a5f389e66d8e
aleksandr kozhemyakin <AKozhemyakin@DBDC.luxoft.com>
2017-01-12 14:41:51 +0300

    comment

---
+++ src/javaappl/JavaAppl.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaappl;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author akozhemyakin
+ */
+public class JavaAppl {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int m[] = new int[]{11,3,5,77};
+        Arrays.sort(m);
+        System.out.println(Arrays.toString(m));
+    }
+    
+}
