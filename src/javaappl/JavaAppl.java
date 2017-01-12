d548785b9867bfdc71ac6c61daec14886e272b8e
aleksandr kozhemyakin <AKozhemyakin@DBDC.luxoft.com>
2017-01-12 22:21:55 +0300

    first variant

--- src/javaappl/JavaAppl.java
+++ src/javaappl/JavaAppl.java
@@ -6,9 +6,9 @@
 package javaappl;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
- *
  * @author akozhemyakin
  */
 public class JavaAppl {
@@ -17,10 +17,24 @@ public class JavaAppl {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        int m[] = new int[]{11,3,5,77};
-        Arrays.sort(m);
-        System.out.println(Arrays.toString(m));
+//        // TODO code application logic here
+//        int m[] = new int[]{11, 3, 5, 77};
+//        Arrays.sort(m);
+//        System.out.println(Arrays.toString(m));
+
+        int z[] = new int[0];
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            int q = scanner.nextInt();
+            int a1[] = new int[z.length + 1];
+            for (int i = 0; i < z.length; i++) {
+                a1[i] = z[i];
+            }
+            a1[a1.length - 1] = q;
+            z = a1;
+            System.out.println(Arrays.toString(z));
+        }
     }
-    
+
 }
